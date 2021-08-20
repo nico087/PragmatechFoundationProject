21f7116e61e530ceb087600d532638b2826eb74a
nico087 <nijat.baghirov@yahoo.com>
2021-08-20 20:16:38 +0400

    Homework for today - Done

---
+++ Algorithms/homework_js/js/app.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var para = document.createElement("div");
+    para.innerHTML = "This is a paragraph.";
+    document.getElementById("new-div-main").appendChild(para);
+}
