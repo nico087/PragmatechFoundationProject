4d7aad45e00cd550666752d9df1c4e155a7d5bfd
nico087 <nijat.baghirov@yahoo.com>
2021-09-22 21:01:44 +0400

    BackEnd Project - Flask - Done!

--- ProjectBackEnd/app.py
+++ ProjectBackEnd/app.py
@@ -13,5 +13,19 @@ def account():
     return render_template('account.html')
 
 
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/product_details')
+def product_details():
+    return render_template('product_details.html')
+
+
+@app.route('/shop_cart')
+def shop_cart():
+    return render_template('shop_cart.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
