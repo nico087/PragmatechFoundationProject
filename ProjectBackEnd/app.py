8ce81a1eb754ee7e3d7d0cdb5302fcf6427dd178
nico087 <nijat.baghirov@yahoo.com>
2021-09-24 20:58:17 +0400

    Working on SQL Alchemy, Flask

--- ProjectBackEnd/app.py
+++ ProjectBackEnd/app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.sqlite3'
+
+db = SQLAlchemy(app)
+
+class Products(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_title = db.Column(db.String(80), nullable=False)
+    image = db.Column(db.String(80), nullable=False)
+
+
 
 
 @app.route('/')
@@ -25,6 +36,14 @@ def product_details():
 @app.route('/shop_cart')
 def shop_cart():
     return render_template('shop_cart.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
